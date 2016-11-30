e8903d57e8df4a376d59bce5578765d2847e5a18
Atl Arredondo <atlsaber@gmail.com>
2016-11-29 23:22:29 -0800

    some new chapters

---
+++ HistoryLogger.py
@@ -0,0 +1,15 @@
+from keras.callbacks import Callback
+
+class HistoryLogger(Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+        self.acc = []
+        self.val_acc = []
+
+    def on_epoch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
+        self.acc.append(logs.get('acc'))
+        self.val_acc.append(logs.get('val_acc'))
+        
